6c7ab65b0e194e82ccb79482e2dffa9c95a6a65e
mohidqadeer123 <mohidqadeer@iu.edu>
2025-09-25 00:03:26 +0500

    Authors order by translation count

--- tt_gutenberg/authors.py
+++ tt_gutenberg/authors.py
@@ -1,10 +1,32 @@
-from .utils import get_alias_translation_counts
+import pandas as pd
+from .utils import load_authors, load_metadata
 
-def list_authors(by_languages=True, alias=True):
-    """
-    Return author aliases ordered by number of translations.
-    """
-    if by_languages and alias:
-        return get_alias_translation_counts()
-    else:
-        raise NotImplementedError("This exercise only covers alias by language count.")
+
+
+def list_authors(by_languages = True, alias - True):
+    '''Function to list authors in order of translation count'''
+
+    #Load Data
+    authors = load_authors()[['gutenber_author_id' , 'author' , 'alias']]
+    meta = load_metadata
+
+    # Keep required columns
+    req_cols = meta[['gutenber_author_id' , 'language']].drop_duplicates()
+
+    # Count languages per author
+    author_count = (req_cols.groupby('gutenberg_author_id').size().reset_index(name = 'lang_count'))
+
+    # Merge author and alias
+    df = req_cols.merge(authors, on = 'guteneberg_author_id' , how = 'left')
+    df = df.sort_values('language_count' , ascending = False, kind = 'mergesoft')
+
+    # Return required columns
+    name_col = 'alias' if alias else 'author'
+    fin_col = df[name_col]
+    mask = name_col.notna() & (name_col.str.strip() != '') & (name_col.str.upper() != 'NA')
+    return fin_col[mask].tolist()
+    
+
+
+
+
